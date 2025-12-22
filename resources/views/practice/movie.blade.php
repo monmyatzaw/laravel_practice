613de0065c88ad87dc8e4e9f6e626eb3bdb78558
Mon <mon@192.168.1.7>
2025-12-22 11:20:32 +0630

    dd_practice

---
+++ resources/views/practice/movie.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+ <meta charset="utf-8">
+ <title>Movie List</title>
+</head>
+<body>
+	 <h1>Movie List</h1>
+	 <ul>
+		<?php  ?>
+		 <li><?php echo $movies['title'] ?></li>
+		 <?php  ?>
+	 </ul>
+</body>
+</html>
+
+<!-- foreach($movies as $movie): endforeach -->
